1173e0a8b0fd60e8673a10259b3edf97e519dea4
deepeshshrestha <ddeepeshh@outlook.com>
2017-12-26 13:12:27 +0545

    added onClickListeners for new form buttons

--- app/src/main/java/np/edu/ku/itmeet/registration.java
+++ app/src/main/java/np/edu/ku/itmeet/registration.java
@@ -139,7 +139,62 @@ public class registration extends Fragment {
             }
         });
 
+        
+        root.findViewById(R.id.btn_data_local).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                bundle.putString("event", "data_local");
+                Fragment fragment = new EventRegistration();
+                fragment.setArguments(bundle);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.main_fragment, fragment);
+                ft.commit();
+            }
+        });
+
+        root.findViewById(R.id.btn_googling).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bundle.putString("event", "googling");
+                Fragment fragment = new EventRegistration();
+                fragment.setArguments(bundle);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.main_fragment, fragment);
+                ft.commit();
+            }
+        });
+
+
+        root.findViewById(R.id.btn_csgo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bundle.putString("event", "csgo");
+                Fragment fragment = new EventRegistration();
+                fragment.setArguments(bundle);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.main_fragment, fragment);
+                ft.commit();
+            }
+        });
+
+
+        root.findViewById(R.id.btn_dota).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bundle.putString("event", "dota");
+                Fragment fragment = new EventRegistration();
+                fragment.setArguments(bundle);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.main_fragment, fragment);
+                ft.commit();
+            }
+        });
+
+        
         return root;
     }
 

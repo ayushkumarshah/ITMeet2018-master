0ff171784d317246a87112b1d4873c4cdf6abd41
deepeshshrestha <ddeepeshh@outlook.com>
2017-12-26 22:08:16 +0545

    registration UI updated

--- app/src/main/java/np/edu/ku/itmeet/EventRegistration.java
+++ app/src/main/java/np/edu/ku/itmeet/EventRegistration.java
@@ -38,14 +38,19 @@ import java.util.Map;
 public class EventRegistration extends Fragment {
 
     List<TextView> tv = new ArrayList<TextView>();
+    List<TextView> th = new ArrayList<TextView>();
     List<LinearLayout> lm = new ArrayList<LinearLayout>();
     List<EditText> et = new ArrayList<EditText>();
+
     List<Spinner> sp = new ArrayList<Spinner>();
+    TextView title_l;
 
     ArrayAdapter<CharSequence> spinner;
     ProgressDialog progressDialog;
 
+    String title="";
     String[] fields={};
+    String[] help={};
     String[] keys={};
     String[] types={};
     String url="";
@@ -80,6 +85,7 @@ public class EventRegistration extends Fragment {
         progressDialog = new ProgressDialog(getActivity());
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Sumbitting...");
+        title_l=root.findViewById(R.id.registration_title);
 
         initializeUI(root);
         queue = Volley.newRequestQueue(getActivity());
@@ -93,85 +99,112 @@ public class EventRegistration extends Fragment {
 
         switch(event){
             case "alumni_meet":
+                title=event_constants.alumni_meet_title;
                 fields=event_constants.alumni_meet_fields;
+                help=event_constants.alumni_meet_help;
                 keys=event_constants.alumni_meet_keys;
                 types=event_constants.alumni_meet_types;
                 url=event_constants.alumni_meet_url;
                 break;
             case "yomari_code_camp":
+                title=event_constants.code_camp_title;
                 fields=event_constants.code_camp_fields;
+                help=event_constants.code_camp_help;
                 keys=event_constants.code_camp_keys;
                 types=event_constants.code_camp_types;
                 url=event_constants.code_camp_url;
                 break;
             case "hackathon":
+                title=event_constants.hackathon_title;
                 fields=event_constants.hackathon_fields;
+                help=event_constants.hackathon_help;
                 keys=event_constants.hackathon_keys;
                 types=event_constants.hackathon_types;
                 url=event_constants.hackathon_url;
                 break;
             case "career_fair":
+                title=event_constants.career_fair_title;
+                help=event_constants.career_fair_help;
                 fields=event_constants.career_fair_fields;
                 keys=event_constants.career_fair_keys;
                 types=event_constants.career_fair_types;
                 url=event_constants.career_fair_url;
                 break;
             case "hardware_comp":
+                title=event_constants.hardware_comp_title;
+                help=event_constants.hardware_comp_help;
                 fields=event_constants.hardware_comp_fields;
                 keys=event_constants.hardware_comp_keys;
                 types=event_constants.hardware_comp_types;
                 url=event_constants.hardware_comp_url;
                 break;
             case "software_comp":
+                title=event_constants.software_comp_title;
+                help=event_constants.software_comp_help;
                 fields=event_constants.software_comp_fields;
                 keys=event_constants.software_comp_keys;
                 types=event_constants.software_comp_types;
                 url=event_constants.software_comp_url;
                 break;
             case "coding_comp":
+                title=event_constants.coding_comp_title;
+                help=event_constants.coding_comp_help;
                 fields=event_constants.coding_comp_fields;
                 keys=event_constants.coding_comp_keys;
                 types=event_constants.coding_comp_types;
                 url=event_constants.coding_comp_url;
                 break;
             case "design_comp":
+                title=event_constants.design_comp_title;
+                help=event_constants.design_comp_help;
                 fields=event_constants.design_comp_fields;
                 keys=event_constants.design_comp_keys;
                 types=event_constants.design_comp_types;
                 url=event_constants.design_comp_url;
                 break;
             case "nephack":
+                title=event_constants.nephack_title;
+                help=event_constants.nephack_help;
                 fields=event_constants.nephack_fields;
                 keys=event_constants.nephack_keys;
                 types=event_constants.nephack_types;
                 url=event_constants.nephack_url;
                 break;                        
             case "data_local":
+                title=event_constants.data_local_title;
+                help=event_constants.data_local_help;
                 fields=event_constants.data_local_fields;
                 keys=event_constants.data_local_keys;
                 types=event_constants.data_local_types;
                 url=event_constants.data_local_url;
                 break;
             case "googling":
+                title=event_constants.googling_title;
+                help=event_constants.googling_help;
                 fields=event_constants.googling_fields;
                 keys=event_constants.googling_keys;
                 types=event_constants.googling_types;
                 url=event_constants.googling_url;
                 break;
             case "csgo":
+                title=event_constants.csgo_title;
+                help=event_constants.csgo_help;
                 fields=event_constants.csgo_fields;
                 keys=event_constants.csgo_keys;
                 types=event_constants.csgo_types;
                 url=event_constants.csgo_url;
                 break;
             case "dota":
+                title=event_constants.dota_title;
+                help=event_constants.dota_help;
                 fields=event_constants.dota_fields;
                 keys=event_constants.dota_keys;
                 types=event_constants.dota_types;
                 url=event_constants.dota_url;
                 break;
         }
-        
+
+        title_l.setText(title+" Registration");
         
         final SharedPreferences mSharedPreference= PreferenceManager.getDefaultSharedPreferences(getActivity());
         name_x=(mSharedPreference.getString("name",null ));
@@ -313,11 +346,11 @@ public class EventRegistration extends Fragment {
         for(int i=0;i<16;i++){
             if(fields[i]!=null) {
             tv.get(i).setText(fields[i]);
+            th.get(i).setText(help[i]);
             lm.get(i).setVisibility(View.VISIBLE);
             switch (types[i]) {
                 case "text":
                     et.get(i).setVisibility(View.VISIBLE);
-
                     break;
                 case "drop":
                     sp.get(i).setVisibility(View.VISIBLE);
@@ -399,11 +432,13 @@ public class EventRegistration extends Fragment {
 
     private void initializeUI(View root) {
         int[] tvid={R.id.t0,R.id.t1,R.id.t2,R.id.t3,R.id.t4,R.id.t5,R.id.t6,R.id.t7,R.id.t8,R.id.t9,R.id.t10,R.id.t11,R.id.t12,R.id.t13,R.id.t14,R.id.t15};
+        int[] thid={R.id.th0,R.id.th1,R.id.th2,R.id.th3,R.id.th4,R.id.th5,R.id.th6,R.id.th7,R.id.th8,R.id.th9,R.id.th10,R.id.th11,R.id.th12,R.id.th13,R.id.th14,R.id.th15};
         int[] lmid={R.id.lm0,R.id.lm1,R.id.lm2,R.id.lm3,R.id.lm4,R.id.lm5,R.id.lm6,R.id.lm7,R.id.lm8,R.id.lm9,R.id.lm10,R.id.lm11,R.id.lm12,R.id.lm13,R.id.lm14,R.id.lm15};
         int[] etid={R.id.e0,R.id.e1,R.id.e2,R.id.e3,R.id.e4,R.id.e5,R.id.e6,R.id.e7,R.id.e8,R.id.e9,R.id.e10,R.id.e11,R.id.e12,R.id.e13,R.id.e14,R.id.e15};
         int[] spid={R.id.s0,R.id.s1,R.id.s2,R.id.s3,R.id.s4,R.id.s5,R.id.s6,R.id.s7,R.id.s8,R.id.s9,R.id.s10,R.id.s11,R.id.s12,R.id.s13,R.id.s14,R.id.s15};
         for(int i=0;i<16;i++){
             tv.add((TextView) root.findViewById(tvid[i]));
+            th.add((TextView) root.findViewById(thid[i]));
             lm.add((LinearLayout) root.findViewById(lmid[i]));
             et.add((EditText) root.findViewById(etid[i]));
             sp.add((Spinner) root.findViewById(spid[i]));

eaf5ee54e70a1c7dfad7c930a9780132e87dc639
deepeshshrestha <ddeepeshh@outlook.com>
2017-12-27 05:30:02 +0545

    Add files via upload

--- app/src/main/java/np/edu/ku/itmeet/event_constants.java
+++ app/src/main/java/np/edu/ku/itmeet/event_constants.java
@@ -5,72 +5,93 @@ package np.edu.ku.itmeet;
  */
 
 public class event_constants {
-
+    public final static String alumni_meet_title="Alumni Meet";
  public final static String[] alumni_meet_fields={"Your Name *","Batch *","Group *","Email *","Contact Number","Are you in Nepal? *","Are you attending the Alumni Meet? *","","","","","","","","","","","","","",""};
+    public final static String[] alumni_meet_help={"","","","","","","","","","","","","","","","","","","",""};
     public final static String[] alumni_meet_keys ={"entry.281272404","entry.389532991","entry.128506278","entry.400202096","entry.1026731878","entry.720258008","entry.2070413220","","","","","","","","","","","","","",""};
     public final static String[] alumni_meet_types ={"text","drop","drop","text","text","drop","drop","","","","","","","","","","","","","",""};
     public final static String   alumni_meet_url ="https://docs.google.com/forms/d/e/1FAIpQLSf6-GQB8fWv0kAlPXyJYiXtIoTotPDN1IbHvMryP1BocEAc8A/formResponse";
 
+    public final static String code_camp_title="Yomari Code Camp";
     public final static String[] code_camp_fields={"Team Name *","Team Members *","College *","Project Theme *","Project Name *","Idea *","Contact Number *","Email *","","","","","","","","","","","","",""};
+    public final static String[] code_camp_help={"","Please use comma to separate names.","Please use comma to separate names if needed.","","Please provide the title of your Project","Please provide a brief and precise description of your conceived project.","","","","","","","","","","","","","",""};
     public final static String[] code_camp_keys={"entry.2092238618","entry.531306590","entry.479301265","entry.1167590725","entry.901361691","entry.981863726","entry.491412188","entry.1556369182","","","","","","","","","","","","",""};
     public final static String[] code_camp_types={"text","text","text","drop","text","text","text","text","","","","","","","","","","","","",""};
     public final static String   code_camp_url="https://docs.google.com/forms/d/16oCuOU19t1gJzOtnf0LYkCi_yi7zIppmBN-QQGvOWNY/formResponse";
 
+    public final static String hackathon_title="Hackathon";
     public final static String[] hackathon_fields={"Team Name *","College ","Team Lead Name, Contact and Email *","Team Members' Name, Contact and Email *","","","","","","","","","","","","","","",""};
+    public final static String[] hackathon_help={"","","e.g. Kiran Kumar Chaudhary - 9842878755 / hellokiran2020@gmail.com","e.g. 1) Rishi Raj Gautam - 9818482284 / thunderfrost@gmail.com (2) xyz - 9898383738 / something@gmail.com","","","","","","","","","","","","","","",""};
     public final static String[] hackathon_keys={"entry.1892380463","entry.716376001","entry.1044814679","entry.1044814679","","","","","","","","","","","","","","","","",""};
     public final static String[] hackathon_types={"text","text","text","text","","","","","","","","","","","","","","","","",""};
     public final static String   hackathon_url="https://docs.google.com/forms/d/e/1FAIpQLSdt769cg7_6ygyWl-ebnz7PB86dpQ7ehB6ALWRC0T2-1qW5vw/formResponse";
 
+    public final static String career_fair_title="Career Fair";
     public final static String[] career_fair_fields={"Email address *","Your Name *","Gender *","Contact Number *","Age *","Are you a ? *","Other:","If you are a college student, Mention the name of your College .","Which course are you taking or have completed? (like BE Computer Engineering, BSc CSIT, BIT etc.) *","Semester","What are your skills? (For eg. Front end web development, Mobile apps development, Network security, Graphic design, ML , non-technical skills, etc.) *","Do you have any previous job experience? *","What do you think is suitable for you to work on? *","Other:","Which companies do you wish to meet in this event?	","What are your suggestions to make this event successful?","","","","",""};
+    public final static String[] career_fair_help={"","","","","","","","","","","","","","","","","","","","",""};
     public final static String[] career_fair_keys={"emailAddress","entry.1272278951","entry.868442742","entry.1317813348","entry.1483629403","entry.78086668","entry.78086668.other_option_response","entry.1672615270","entry.1366562968","entry.856445358","entry.2086499191","entry.1874113236","entry.134499872","entry.134499872.other_option_response","entry.1335805437","entry.74578925","","","","",""};
     public final static String[] career_fair_types={"text","text","drop","text","drop","drop","text","text","text","drop","text","drop","drop","text","text","text","","","","",""};
     public final static String   career_fair_url="https://docs.google.com/forms/d/e/1FAIpQLSeEnNb7_cvx3SpBDHhtNz23njhA_gZpjy6aIUG65EEcItYu-g/formResponse";
 
+    public final static String software_comp_title="Software Competition";
     public final static String[] software_comp_fields={"Email address *","Your Name *","Contact Number *","Name of your team members(separated by comma) *","Name of your Institution(if any)/College *","Current Level (+2, Bachelor, Masters, Pass out, Others.Include Year and/or semester if relevant) *","Project Name *","Project Description *","Programming Language Used *","Target Audience *","","","","","","",""};
+    public final static String[] software_comp_help={"","","","","","","","","","","","","","","","","","","","",""};
     public final static String[] software_comp_keys={"emailAddress","entry.891811928","entry.142700828","entry.1912727949","entry.1280039454","entry.600176852","entry.1523340891","entry.1683898638","entry.1795338347","entry.1497026135","","","","","","","","","","",""};
     public final static String[] software_comp_types={"text","text","text","text","text","text","text","text","text","text","","","","","","","","","","",""};
     public final static String   software_comp_url="https://docs.google.com/forms/d/e/1FAIpQLSdPhmX1n7kmSxMUKt0OkJbanZb5sttNj9AoZYB6KGAzFtDGQQ/formResponse";
 
+    public final static String hardware_comp_title="Hardware Competition";
     public final static String[] hardware_comp_fields={"Email address *","Your Name *","Contact Number *","Name of your team members(separated by comma) *","Name of your Institution(if any)/College *","Current Level (+2, Bachelor, Masters, Pass out, Others.Include Year and or semester if relevant) *","Project Name *","Project Description *","Hardware Components Used *","Programming Language Used(if any) *","Target Audience *","","","","","",""};
+    public final static String[] hardware_comp_help={"","","","","","","","","","","","","","","","","","","","",""};
     public final static String[] hardware_comp_keys={"emailAddress","entry.826464218","entry.1002128832","entry.1084317527", "entry.1046420040","entry.473304277","entry.138601701","entry.17874778", "entry.1193348225" ,"entry.1952142105","entry.1197730287","","","","","","","","","",""};
     public final static String[] hardware_comp_types={"text","text","text","text","text","text","text","text","text","text","text","","","","","","","","","",""};
     public final static String   hardware_comp_url="https://docs.google.com/forms/d/e/1FAIpQLSfcJ5SGGkTzwOoNhI3hH7p-uinUxzGyvv6AWbW1VsRgPWNBtQ/formResponse";
 
-
-
+    public final static String nephack_title="NepHack Penetration";
     public final static String[] nephack_fields={"Team name *", "Number of members *", "College /Institution", "Team Lead Name, Contact and Email *","Team Members' Name, Contact and Email *", "Programming Knowledge *", "How do you catagorize yourself ? *", "What are your views about present cyber security condition of nepal & what do you think should be improved to enhance the security?" ,"","","","","","","","","","","","","","","","","","","","","","","",""};
+    public final static String[] nephack_help={"","","","","For Example: 1. Uttam Sapkota, 9845636944, xyz@gmail.com 2. Romit Khanal, 9862038982, xyz@gmail.com","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
     public final static String[] nephack_keys={  "entry.1653126513", "entry.309051150","entry.989784428", "entry.683855677", "entry.217136929",  "entry.836187324","entry.2001496452","entry.1787498934","","","","","","","","","","","","","","","","","","","","","","","",""};
     public final static String[] nephack_types={"text","text","text","text","text","text","drop","text","","","","","","","","","","","","","","","","","","","","","","","",""};
     public final static String nephack_url="https://docs.google.com/forms/d/e/1FAIpQLSdv_sixId7uQsjNOJ-9UqfUohI_SKdP5hdhcW5afybCFfgRXQ/formResponse" ;
 
+    public final static String coding_comp_title="Coding Competition";
     public final static String[] coding_comp_fields={"Team Name *","College /University Name *","Team Leader, Email and Contact *","Other team Member, Email and Contact *","","","","","","","","","","","","","","","",""};
+    public final static String[] coding_comp_help={"","","e.g 1.kaflemahesh0@gmail.com/9843626602","e.g 2.saugatadh91@gmail.com/9843407264 , 3.xyz@gmail.com/98********","","","","","","","","","","","","","","",""};
     public final static String[] coding_comp_keys={"entry.2125498593","entry.1838799553" , "entry.1826882690","entry.917954886","","","","","","","","","","","","","","","",""};
     public final static String[] coding_comp_types={"text","text","text","text","","","","","","","","","","","","","","","",""};
     public final static String coding_comp_url="https://docs.google.com/forms/d/e/1FAIpQLSc7GUE1UTttnyJvZcB-XUHSAbAgM6-a3Gk_mI2R_6nTrCVYUQ/formResponse";
 
+    public final static String design_comp_title="Design Competition";
     public final static String[] design_comp_fields={"Your Name *","Email Address *","Address *","Contact Number *","Education Background","Other","Platform(s) you are likely to use","Other","How did you hear about this event?","","","","","","","","","","","",""};
+    public final static String[] design_comp_help={"","","","","","","","","","","","","","","","","","","",""};
     public final static String[] design_comp_keys={"entry.877086558","entry.1498135098","entry.394988351","entry.2094424829" ,  "entry.558591488","entry.558591488.other_option_response","entry.1783281921","entry.1783281921.other_option_response", "entry.1424661284" ,"","","","","","","","","","","",""};
     public final static String[] design_comp_types={"text","text","text","text","drop","text","drop","text","drop","","","","","","","","","","","",""};
     public final static String design_comp_url= "https://docs.google.com/forms/d/e/1FAIpQLSdQZ4FENi3Im-sPp51d2RBKi6KUbViXAi3LP7LFsnMf9YkWqg/formResponse";
 
+    public final static String data_local_title="Datathon and Localization";
     public final static String[] data_local_fields={"Your Name *","Email *","Address ","Contact Number *","What is your preference? *","Name of institution (if any)/College ","Current Level (+2, Bachelor, Masters, Pass out, Others. Include Year and/or semester if relevant) ","Any Queries or Comments. (Not Compulsory)","","","","","","","","","","","",""};
+    public final static String[] data_local_help={"","","","","","","","","","","","","","","","","","","",""};
     public final static String[] data_local_keys={"entry.2005620554","entry.1045781291","entry.1065046570","entry.1166974658","entry.93870404","entry.446515507","entry.957718101","entry.839337160","","","","","","","","","","","",""};
     public final static String[] data_local_types={"text","text","text","text","drop","text","text","text","","","","","","","","","","","",""};
     public final static String data_local_url= "https://docs.google.com/forms/d/e/1FAIpQLSetLh4ZRRIA60GhWKO4kgaqLVOKWHjc_vn_MAhw3WZJvTCYvQ/formResponse";
 
+    public final static String googling_title="Googling";
     public final static String[] googling_fields={"Your Name *","College *","Contact Number *","Email *","","","","","","","","","","","","","","","",""};
+    public final static String[] googling_help={"","","","","","","","","","","","","","","","","","","",""};
     public final static String[] googling_keys={"entry.156641866" ,"entry.1925217576","entry.383490558","entry.5184296","","","","","","","","","","","","","","","",""};
     public final static String[] googling_types={"text","text","text","text","","","","","","","","","","","","","","","",""};
     public final static String googling_url="https://docs.google.com/forms/d/e/1FAIpQLSexN1j1ES4YhMI9KQHjSFf5XSiBg18rLuzDyWah0PXcOoz8ZA/formResponse" ;
 
+    public final static String csgo_title="CS:GO";
     public final static String[] csgo_fields={"Name of the team *","Name , phone number and email of Group Leader *","Name , phone number and email of the Players *","Does your team have steam account? *","Payment Type *","Location of payment","Previous competition participated in (if any)","Any suggestions ?","","","","","","","","","","","",""};
+    public final static String[] csgo_help={"","eg: 1. Sagun Pandit/ 9843065974 / sagun@gmail.com","eg: 2. Aayush Ratna Sthapit/ 9840069089 / aayush@gmail.com","","","","","","","","","","","","","","","","",""};
     public final static String[] csgo_keys={"entry.1498135098","entry.2606285","entry.1424661284","entry.30431091","entry.822160592","entry.613705229","entry.1531100140","entry.1390419333","","","","","","","","","","","",""};
     public final static String[] csgo_types={"text","text","text","drop","drop","drop","text","text","","","","","","","","","","","",""};
-    public final static String csgo_url= "https://docs.google.com/forms/d/e/1FAIpQLSdlEpXJ7KYO4DoB5esj5wHo_CptWcFM_ZnRsQLOigXH-Ko46w/formResponse"
-            ;
+    public final static String csgo_url= "https://docs.google.com/forms/d/e/1FAIpQLSdlEpXJ7KYO4DoB5esj5wHo_CptWcFM_ZnRsQLOigXH-Ko46w/formResponse";
 
-    public final static String[] dota_fields={"Name of the team *","Name , phone number and email of Group Leader *","Name , phone number and email of the Players *","Payment Type *","Location of payment","Previous competition participated in (if any)","Any suggestions ?","","","","","","","","","","","","",""
-    };
+    public final static String dota_title="DOTA 2";
+    public final static String[] dota_fields={"Name of the team *","Name , phone number and email of Group Leader *","Name , phone number and email of the Players *","Payment Type *","Location of payment","Previous competition participated in (if any)","Any suggestions ?","","","","","","","","","","","","",""};
+    public final static String[] dota_help={"","eg: 1. Pratik Dhungel / 9840065180 / pratik@gmail.com","eg: 2. Sanjib Acharya / 9849022777 / sanjib@gmail.com","","","","","","","","","","","","","","","","",""};
     public final static String[] dota_keys={"entry.1498135098","entry.2606285","entry.1424661284","entry.822160592","entry.613705229","entry.1531100140","entry.1390419333","","","","","","","","","","","","",""};
     public final static String[] dota_types={"text","text","text","drop","drop","text","text","","","","","","","","","","","","",""};
     public final static String dota_url="https://docs.google.com/forms/d/e/1FAIpQLSdmukxliplG36BlqIgGEUJG3iNfVFcSCdx1QCbanyO553xt2A/formResponse" ;
